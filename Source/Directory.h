9199a176da8652b4d778348c22ac2f09f44a6ce5
Adam Wilson <adam@codegarden.co.uk>
2015-12-09 01:22:44 +0000

    first iteration - get module info from Cloudant DB

--- Source/Directory.h
+++ Source/Directory.h
@@ -9,35 +9,30 @@
 #include <iostream>
 
 
-/** Decodes module names in the format repo/name@version */
-class ModuleName
-{
-public:
-    ModuleName (const String& name_) : name (name_) {}
+	class Iterator
+	{
+	public:
+		Iterator(ValueTree & tree, int position) : tree(tree), pos(position) {}
+		Iterator & operator++() { ++pos; return *this; }
+		bool operator!= (const Iterator & other) const { return other.pos != pos || other.tree != tree; }
+		ValueTree operator * () const { return tree.getChild(pos); }
 
-    String getRepo() const
-    {
-        return name.contains ("/") ? name.upToFirstOccurrenceOf ("/", false, false) : String::empty;
-    }
+	private:
+		Iterator & operator=(const Iterator &) = delete;
+		ValueTree & tree;
+		int pos;
+	};
 
-    String getVersion() const
-    {
-        return name.contains ("@") ? name.fromLastOccurrenceOf ("@", false, false) : String::empty;
-    }
+	Iterator begin() { return Iterator(tree, 0); }
+	Iterator end() { return Iterator(tree, tree.getNumChildren()); }
 
-    String getName() const
-    {
-        if (name.contains ("/"))
-            return name.fromFirstOccurrenceOf ("/", false, false).upToLastOccurrenceOf ("@", false, false);
-
-        return name.upToLastOccurrenceOf ("@", false, false);
-    }
 private:
-    String name;
+	JUCE_DECLARE_NON_COPYABLE(ValueTreeChildrenConnector)
+		ValueTree tree;
 };
 
-/** Holds an index of where to find specific modules.  We may want more than
- * one of these in the end. */
+
+/** Holds an index of where to find specific modules.  We may want more than one of these in the end. */
 class Directory
 {
 public:
@@ -53,94 +48,94 @@ public:
      * description - description
      * subpath - again source dependent but used for finding the module
      */
-
+	
     /**
      * Opens a directory with the given URL, downloading the latest version of
      * the contents. 
-     */
-    Directory (URL location)
-    {
-        DownloadCache cache;
-        String file = cache.downloadTextFile (location);
-        ScopedPointer<XmlElement> xml = XmlDocument (file).getDocumentElement();
+	*/
+	Directory(URL location)
+	{
+		DownloadCache cache;
+		String file = cache.downloadTextFile(location);
+		ScopedPointer<XmlElement> xml = XmlDocument(file).getDocumentElement();
 
-        if (xml)
+		if (xml)
         {
-            directory = ValueTree::fromXml (*xml);
+			directory = ValueTree::fromXml(*xml);
         }
-        else
+		else
         {
             throw JpmFatalExcepton ("directory format error or network problem",
                                     "Check " 
                                     + cache.getCachedFileLocation (location).getFullPathName() 
                                     + " for debugging which should contain the contents of " 
                                     + location.toString (true));
-        }
+	}
     }
 
 
-    /** 
+	/** 
      * Find all the modules with a specific name.  moduleName can be specified
      * as to match a specific version in a specific repo: juce/juce_core@3.1.1
      * In this case it should match one entry, but there's no guarantee of
      * that!
-    */
-    Array<Module> getModulesByName (const String& moduleNameString)
-    {
-        Array<Module> results;
-        ModuleName module (moduleNameString);
+	*/
+	Array<Module> getModulesByName(const String & moduleNameString)
+	{
+		Array<Module> results;
+		ModuleName module(moduleNameString);
 
         for (auto repoEntry : ValueTreeChildrenConnector (directory))
-        {
+		{
             if (module.getRepo().isEmpty() || repoEntry["shortname"] == module.getRepo())
-                results.addArray (getMatchingModulesFromRepo (repoEntry, module.getName()));
-        }
+			results.addArray(getMatchingModulesFromRepo(repoEntry, module.getName()));
+		} 
 
-        /* If a version was provided then set it.  We won't know until download time whether it definitely exists. */
-        if (module.getVersion().isNotEmpty())
-            for (auto& a : results)
-                a.setVersion (module.getVersion());
+		/* If a version was provided then set it.  We won't know until download time whether it definitely exists. */
+		if (module.getVersion().isNotEmpty())
+			for (auto & a : results)
+				a.setVersion(module.getVersion());
 
-        return results;
-    }
+		return results;
+	}
 
 private:
-    Array<Module> getMatchingModulesFromRepo (ValueTree repoEntry, const String& name)
-    {
-        Array<Module> result;
+	Array<Module> getMatchingModulesFromRepo(ValueTree repoEntry, const String & name)
+	{
+		Array<Module> result;
 
-        for (auto moduleEntry : ValueTreeChildrenConnector (repoEntry))
-        {
-            if (moduleEntry["name"].toString().matchesWildcard (name, false))
-            {
+		for (auto moduleEntry: ValueTreeChildrenConnector(repoEntry))
+		{
+			if (moduleEntry["name"].toString().matchesWildcard(name, false))
+			{
                 /* We use this short lambda for validating the mandatory fields
                  * in the directory. */
-                auto test = [&repoEntry] (const String & text)
-                {
-                    if (text.isEmpty())
+				auto test = [&repoEntry](const String & text)
+				{
+					if (text.isEmpty())
                         printWarning("warning: error in directory for repo " + repoEntry["shortname"].toString());
 
-                    return text;
-                };
+					return text;
+				};
 
-                Module m;
-
-                m.setRepo (test (repoEntry["shortname"]));
-                m.setPath (test (repoEntry["path"]));
-                m.setSource (test (repoEntry["source"]));
-                m.setName (test (moduleEntry["name"]));
-                m.setSubPath (test (moduleEntry["subpath"]));
-
+				Module m;
+                
+				m.setRepo(test(repoEntry["shortname"]));
+				m.setPath(test(repoEntry["path"]));
+				m.setSource(test(repoEntry["source"]));
+				m.setName(test(moduleEntry["name"]));
+				m.setSubPath(test(moduleEntry["subpath"]));
+                
                 /* Description is allowed to be empty. */
-                m.setDescription (moduleEntry["description"]);
+				m.setDescription(moduleEntry["description"]);
+                
+				result.add(m);
+			}
+		}
 
-                result.add (m);
-            }
-        }
-
-        return result;
-    }
-    ValueTree directory;
+		return result;
+	}
+	ValueTree directory;
 };
 
 #endif  // REPOSITORY_H_INCLUDED
--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,4 +1,4 @@
-/*
+/* 
 	This program is free software : you can redistribute it and / or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
@@ -19,82 +19,83 @@
 #include "ModuleGenerator.h"
 #include "Module.h"
 #include "Directory.h"
+#include "Database.h"
 #include "ConfigFile.h"
 
 class App
 {
 public:
-    App (StringArray commandLine_)
-        :
-        config (File::getCurrentWorkingDirectory().getChildFile ("jpmfile.xml")),
-        commandLine (commandLine_)
-    {
-        jucer.setFile (File::getCurrentWorkingDirectory());
-    }
+	App(StringArray commandLine_)
+		:
+		config(File::getCurrentWorkingDirectory().getChildFile("jpmfile.xml")),
+		commandLine(commandLine_)
+	{
+		jucer.setFile(File::getCurrentWorkingDirectory());
+	}
 
-    ~App()
-    {
-        jucer.save();
-    }
+	~App()
+	{
+		jucer.save();
+	}
 
 
-    void run()
-    {
+	void run()
+	{
         printInfo ("juce package manager");
-        String command = commandLine[1];
-        commandLine.removeRange (0, 2);
+		String command = commandLine[1];
+		commandLine.removeRange(0, 2); 
 
-        if (command == "list")
-            list();
+		if (command == "list")
+			list();
         else if (command == "install")
-            install();
+			install();
         else if (command == "genmodule")
-            genmodule();
+			genmodule();
         else if (command == "rebuildjucer")
-            rebuildJucerModuleList();
+			rebuildJucerModuleList();
         else if (command == "erasecache")
-            DownloadCache().clearCache();
+			DownloadCache().clearCache();
         else if (command == "add")
             add();
         else
-            printError ("command not found");
-    }
+            printError("command not found"); 
+	}
 
 private:
-    /** Add a modules to the jpmfile.xml and install it. */
-    void addModuleFromDirectory (const String& moduleName)
-    {
+	/** Add a modules to the jpmfile.xml and install it. */
+	void addModuleFromDirectory(const String & moduleName)
+	{
         Directory directory (directoryUrl);
-        auto modules = directory.getModulesByName (moduleName);
+		auto modules = directory.getModulesByName(moduleName);
 
-        if (modules.size() == 0)
-        {
-            std::cerr << "error: module " << moduleName << " not found" << std::endl;
-            return;
-        }
+		if (modules.size() == 0)
+		{
+			std::cerr << "error: module " << moduleName << " not found" << std::endl;
+			return;
+		}
 
-        if (modules.size() > 1)
+		if (modules.size() > 1)
             printInfo ("installing " + String (modules.size()) + " modules");
 
-        for (auto module : modules)
+        for (auto module: modules)
         {
             printHeading ("installing: " + module.getRepo() + "/" + module.getName() + "@" + module.getVersion());
-
-            if (module.isValid())
-            {
-                module.install (File::getCurrentWorkingDirectory().getChildFile ("jpm_modules"));
-                config.addModule (module);
-            }
+            
+    		if (module.isValid())
+    		{
+                module.install(File::getCurrentWorkingDirectory().getChildFile("jpm_modules"));
+                config.addModule(module);
+    		}
         }
-
-        rebuildJucerModuleList();
-    }
-
-
-    void addLocalModule (const String& userSuppliedPath)
-    {
-        File folder (File::getCurrentWorkingDirectory().getChildFile (userSuppliedPath));
         
+        rebuildJucerModuleList();
+	}
+
+
+    void addLocalModule(const String & userSuppliedPath)
+    {
+        File folder(File::getCurrentWorkingDirectory().getChildFile(userSuppliedPath)); 
+
         if (! folder.exists())
         {
             printError("module " + folder.getFullPathName() + " not found");
@@ -102,109 +103,110 @@ private:
         }
 
         Module m;
-        m.setSource ("LocalPath");
-        m.setName (folder.getFileNameWithoutExtension());
-        m.setVersion ("NA");
+        m.setSource("LocalPath"); 
+        m.setName(folder.getFileNameWithoutExtension());
+        m.setVersion("NA"); 
         /* A path provided by the user is made relative to the project, and without the module
          * folder itself being included to avoid confusing the introjucer. */
-        m.setPath (folder.getParentDirectory().getRelativePathFrom (File::getCurrentWorkingDirectory()));
-        config.addModule (m);
+        m.setPath(folder.getParentDirectory().getRelativePathFrom(File::getCurrentWorkingDirectory())); 
+        config.addModule(m);
 
         rebuildJucerModuleList();
     }
 
-    void addModuleToJucer (Module module)
+    void addModuleToJucer(Module module)
     {
         if (module.getSource() == "LocalPath")
-            jucer.addModule (module.getName(), module.getPath());
+            jucer.addModule(module.getName(), module.getPath()); 
         else
-            jucer.addModule (module.getName());
+            jucer.addModule(module.getName());
     }
 
-    /** Update the jucer file with the latest list of modules. */
-    void rebuildJucerModuleList()
-    {
-        jucer.clearModules();
+	/** Update the jucer file with the latest list of modules. */
+	void rebuildJucerModuleList()
+	{
+		jucer.clearModules();
 
-        auto allModules = config.getModules();
+		auto allModules = config.getModules();
 
-        for (auto m : allModules)
-            addModuleToJucer (m);
-    }
+		for (auto m : allModules)
+			addModuleToJucer(m);
+	}
 
-    /** Installs any modules that are missing from the jpm_modules folder. */
-    void installMissingModules()
-    {
-        auto allModules = config.getModules();
+	/** Installs any modules that are missing from the jpm_modules folder. */
+	void installMissingModules()
+	{
+		auto allModules = config.getModules();
 
-        for (auto module : allModules)
-        {
-            auto jpmModulesFolder = File::getCurrentWorkingDirectory().getChildFile ("jpm_modules");
-            auto name = module.getName();
+		for (auto module : allModules)
+		{
+			auto jpmModulesFolder = File::getCurrentWorkingDirectory().getChildFile("jpm_modules");
+			auto name = module.getName(); 
 
-            if (!jpmModulesFolder.getChildFile (name).exists())
-            {
-                std::cout << "installing: " << name << std::endl;
-                module.install (jpmModulesFolder);
-            }
-        }
-    }
+			if (!jpmModulesFolder.getChildFile(name).exists())
+			{
+				std::cout << "installing: " << name << std::endl;
+				module.install(jpmModulesFolder);
+			}
+		}
+	}
 
-
-    void install()
-    {
-        if (commandLine.size() == 0)
-        {
+    
+	void install()
+	{
+		if (commandLine.size() == 0)
+		{
             printInfo ("installing missing modules");
             printInfo ("to force a refresh delete the jpm_modules folder first)");
-            installMissingModules();
-        }
-        else
-        {
-            while (commandLine.size() > 0)
-            {
-                addModuleFromDirectory (commandLine[0]);
-                commandLine.remove (0);
-            }
-        }
-    }
-
+			installMissingModules();
+		}
+		else
+		{
+			while (commandLine.size() > 0)
+			{
+				addModuleFromDirectory(commandLine[0]);
+				commandLine.remove(0);
+			}
+		}
+	}
+    
     void genmodule()
     {
         ModuleGenerator generator;
-        generator.run (commandLine);
+        generator.run(commandLine);
     }
 
-    void list()
-    {
+	void list()
+	{
         Directory directory (directoryUrl);
-        String searchString;
+		String searchString;
 
-        if (commandLine.size() == 0)
-            searchString = "*";
-        else
-            searchString = commandLine[0];
+		if (commandLine.size() == 0)
+			searchString = "*";
+		else
+			searchString = commandLine[0];
 
-        auto result = directory.getModulesByName (searchString);
+		//auto result = directory.getModulesByName(searchString);
+		auto result = database.getModulesByName(searchString);
 
-        for (auto r : result)
-            std::cout << r.getSummaryString() << std::endl;
-    }
+//		for (auto r: result)
+//			std::cout << r.getSummaryString() << std::endl;
+	}
 
     void add()
     {
         if (commandLine.size() == 0)
-            printError ("You need to specify a folder with a module in it");
+            printError("You need to specify a folder with a module in it"); 
         else
-            addLocalModule (commandLine[0]);
+            addLocalModule(commandLine[0]);
     }
 
-    ConfigFile config;
-    JucerFile jucer;
+	ConfigFile config;
+	JucerFile jucer;	
+	Directory directory;
+    Database database;
 
-    URL directoryUrl { URL ("https://raw.githubusercontent.com/jcredland/jpm/master/jpm_directory.xml") };
-
-    StringArray commandLine;
+	StringArray commandLine;
 };
 
 
@@ -230,31 +232,31 @@ void usage()
 
 int main (int argc, char* argv[])
 {
-    StringArray commandLineArguments;
+	StringArray commandLineArguments;
 
-    for (int i = 0; i < argc; ++i)
-        commandLineArguments.add (argv[i]);
+	for (int i = 0; i < argc; ++i)
+		commandLineArguments.add(argv[i]); 
 
-    if (commandLineArguments.size() <= 1)
-    {
-        usage();
-        return 1;
-    }
+	if (commandLineArguments.size() <= 1)
+	{
+		usage();
+		return 1;
+	}
 
-    /*
-    This mega-try/catch probably isn't the best idea. Though it's a commandline failure and
-    some hard-failure modes are probably not inappropriate.
-    */
+	/* 
+	This mega-try/catch probably isn't the best idea. Though it's a commandline failure and
+	some hard-failure modes are probably not inappropriate. 
+	*/
     try
     {
-        App app (commandLineArguments);
-        app.run();
-    }
-    catch (InvalidJucerFormat)
-    {
-        std::cerr << "exception: invalid jucer file format" << std::endl;
-        return 1;
-    }
+		App app(commandLineArguments);
+		app.run(); 
+	} 
+	catch (InvalidJucerFormat)
+	{
+		std::cerr << "exception: invalid jucer file format" << std::endl;
+		return 1;
+	}
     catch (JpmFatalExcepton e)
     {
         std::cerr << "exception: " << e.error << std::endl;
